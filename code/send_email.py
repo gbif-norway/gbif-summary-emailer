23c35473f9e1795c129eac334a6211ddd58c0636
rukayaj@gmail.com <rukayaj@gmail.com>
2023-12-20 10:34:56 +0200

    Typo

--- code/send_email.py
+++ code/send_email.py
@@ -9,8 +9,8 @@ def send_email(name, email, datasets):
     msg = EmailMessage()
     msg['Subject'] = f'Your {datetime.now().strftime("%b %Y")} GBIF dataset status update'
     msg['From'] = os.getenv('SMTP_EMAIL')
-    msg['To'] = os.getenv('TEST_EMAILS') # email\
-    msg['Bcc'] = 'rukayaj@gmail.com'
+    msg['To'] = email #\
+    msg['Bcc'] = os.getenv('TEST_EMAILS')
     template = Template(open('template.html').read())
     print(len(datasets))
     rendered_html = template.render(name=name, all_datasets_link='https://www.gbif.org/occurrence/search?' + '&'.join([f'dataset_key={k["key"]}' for k in datasets]), datasets=datasets,date=datetime.now().strftime("%B %Y"))
